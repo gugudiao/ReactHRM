e66775408558c2f95ac15b9a20cac7ff401e0b58
刘年 <179337866@qq.com>
2019-03-21 17:34:23 +0800

    将命名按照规范进行修改

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Layoutmain from './layout/demoLayout';
-import Demo_hook from './pages/demo_hook/index.js';
+import DemoHook from './pages/DemoHook/index.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layoutmain/>
-        {/* <Demo_hook /> */}
+        {/* <DemoHook /> */}
       </div>
     )
   }
