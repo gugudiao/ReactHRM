920533fdc40372a875cc7adcccadf9f96aa2fe22
bangv <weibangxu@163.com>
2019-03-21 17:44:09 +0800

    简单的ui组件

--- src/pages/smartManagePage/smartManage.js
+++ src/pages/smartManagePage/smartManage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { DatePicker } from 'antd';
 
+
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 export default class smartManage extends React.Component{
+    
+ onChange =(date, dateString)=> {
+    console.log(date, dateString);
+  }
     render(){
         return (
             <div>
-                <h2>智慧管理</h2>
+                <h2>智慧管理</h2> 
+                 <DatePicker style={{'margin-bottom':'20px'}} onChange={this.onChange} />
+                <br />
+                <MonthPicker style={{'margin-bottom':'20px'}}  onChange={this.onChange} placeholder="Select month" />
+                <br />
+                <RangePicker style={{'margin-bottom':'20px'}}  onChange={this.onChange} />
+                <br />
+                <WeekPicker style={{'margin-bottom':'20px'}}  onChange={this.onChange} placeholder="Select week" />
             </div>
         );
     }
--- src/pages/workerServicePage/workerService.js
+++ src/pages/workerServicePage/workerService.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Button, notification } from 'antd';
 export default class workerService extends React.Component{
     render(){
+        const openNotificationWithIcon = (type,data) => {
+            notification[type]({
+              message: data,
+              description: '我是提示框',
+            });
+          };
         return (
             <div>
                 <h2>员工服务</h2>
+                <Button style={{"width":"100px",display:'block',margin:"0 auto","marginBottom":"20px"}} onClick={() => openNotificationWithIcon('success',"成功提示")}>点我Success</Button>
+                <br/>
+                <Button style={{"width":"100px",display:'block',margin:"0 auto","marginBottom":"20px"}} onClick={() => openNotificationWithIcon('info',"Info提示")}>点我Info</Button>
+                <br/>
+                <Button style={{"width":"100px",display:'block',margin:"0 auto","marginBottom":"20px"}} onClick={() => openNotificationWithIcon('warning',"warning提示")}>点我Warning</Button>
+                <br/>
+                <Button style={{"width":"100px",display:'block',margin:"0 auto","marginBottom":"20px"}} onClick={() => openNotificationWithIcon('error',"error提示")}>点我Error</Button>
             </div>
         );
     }
