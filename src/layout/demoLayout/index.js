ff26069bf372107213f6829131f898a67d8451d6
luoxuyuan <luoxuyuan@126.com>
2019-03-24 11:39:08 +0800

    测试同页面跳转已有的html页面

--- src/layout/demoLayout/index.js
+++ src/layout/demoLayout/index.js
@@ -8,6 +8,30 @@ const Header = Layout.Header;
 const {Footer,Sider,Content} = Layout;
 
  class Layoutmain extends React.Component{
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            display_name: 'none', //此状态机为display的取值
+            display_old: 'block',
+        }
+    }
+    display_name() { //编辑按钮的单击事件，修改状态机display_name的取值
+        if (this.state.display_name == 'none') {
+            this.setState({
+                display_name: 'block',
+                display_old: 'none',
+            })
+        }
+    }
+    handle=()=>{
+        
+        if (this.state.display_name == 'block') {
+            this.setState({
+                display_name: 'none',
+                display_old: 'block',
+            })      
+      }
+    }
     render(){
 
         return (
@@ -23,16 +47,19 @@ const {Footer,Sider,Content} = Layout;
                             defaultSelectedKeys={['1']}
                             style={{ lineHeight: '38px' }}
                         >
-                            <Menu.Item key="1"><Link to={{ pathname: '/workerService'}}>员工服务</Link></Menu.Item>
-                            <Menu.Item key="2"><Link to={{ pathname: '/smartManage'}}>智慧管理</Link></Menu.Item>
-                            <Menu.Item key="3"><Link to={{ pathname: '/workDo'}}>业务处理</Link></Menu.Item>
+                            <Menu.Item key="1"><Link to={{ pathname: '/workerService'}} onClick={this.handle}>员工服务</Link></Menu.Item>
+                            <Menu.Item key="2"><Link to={{ pathname: '/smartManage'}} onClick={this.handle}>智慧管理</Link></Menu.Item>
+                            <Menu.Item key="3"><Link to={{ pathname: '/workDo'}} onClick={this.handle}>业务处理</Link></Menu.Item>
+                            <Menu.Item key="4"><a href="/test.html" target="content" onClick={this.display_name.bind(this)}>test page</a></Menu.Item>
                         </Menu>
                         </Header>
                         <Content style={{ padding: '30px 50px 0px 50px' }}>
-                            <div style={{ background: '#fff', padding: 24, minHeight: 800 }}>
+                            <div style={{ background: '#fff', padding: 24, minHeight: 800, display:this.state.display_old }}>
                             
                                 <Root />
                             </div>
+                            <iframe id="content" style={{ background: '#fff', padding: 24, minHeight: 800, display:this.state.display_name }}>
+                            </iframe>
                         </Content>
                         <Footer style={{ textAlign: 'center' }}>
                             中国工商银行版权所有                                                                        
